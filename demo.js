e6e583c8ac7f940853c782b37472b846c7d9c14e
李宗校 <158638447@qq.com>
2022-08-15 11:16:46 +0800

    test:添加第一个语句

---
+++ demo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         myplugins
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  one plugin!
+// @author       lzx
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  console.log("第一个脚本");
+})();
