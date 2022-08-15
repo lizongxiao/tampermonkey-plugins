48af2ee8ce37ef5a8b254d94b6c7f98eebec1a2a
李宗校 <158638447@qq.com>
2022-08-15 11:41:00 +0800

    test:测试自动更新

--- demo.js
+++ demo.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         myplugins
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  one plugin!
 // @author       lzx
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-//@license       AGPL -3.0
+// @license       AGPL -3.0
 // @grant        none
 // ==/UserScript==
 
 (function () {
   "use strict";
 
-  console.log("脚本");
+  console.log("第一个脚本");
 })();
