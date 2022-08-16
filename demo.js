613386017c5f911a49a1920f791b8ce39de7e2eb
李宗校 <158638447@qq.com>
2022-08-16 17:11:25 +0800

    测试->新增掘金签到功能

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@
 // @version      0.3
 // @description  one plugin!
 // @author       lzx
-// @match        https://www.google.com/
+// @match        https://juejin.cn/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @license       AGPL -3.0
 // @grant        none
@@ -13,6 +13,36 @@
 
 (function () {
   "use strict";
+  const storageKey = "last_sign_timestamp";
+  // 获取上一次签到的日子
+  const lastSignNumberOfDays = GM_getValue(storageKey, 0);
+  // 计算现在所在的日子
+  const currentNumberOfDays = Math.floor(
+    new Date().valueOf() / 1000 / 60 / 60 / 24
+  );
 
-  console.log("脚本");
+  // 如果今天已经请求过，不再请求
+  if (currentNumberOfDays !== lastSignNumberOfDays) {
+    GM_xmlhttpRequest({
+      url: "https://api.juejin.cn/growth_api/v1/check_in",
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "user-agent": navigator.userAgent,
+      },
+      responseType: "json",
+      onload(response) {
+        if (response.status === 200) {
+          const data = response.response;
+          if (data.data === "success") {
+            alert("签到成功");
+          } else {
+            alert(data.err_msg);
+          }
+          // 更新最近一次签到的日子
+          GM_setValue(storageKey, currentNumberOfDays);
+        }
+      },
+    });
+  }
 })();
