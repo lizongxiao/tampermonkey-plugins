18f0086c2db46576ca9171477e57723bf9772a64
李宗校 <158638447@qq.com>
2022-08-16 15:16:07 +0800

    test=>test

--- demo.js
+++ demo.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         myplugins
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  one plugin!
 // @author       lzx
-// @match        *://*
+// @match        https://www.google.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @license       AGPL -3.0
 // @grant        none
+// @require      http://code.jquery.com/jquery-migrate-1.2.1.min.js
 // ==/UserScript==
 
 (function () {
