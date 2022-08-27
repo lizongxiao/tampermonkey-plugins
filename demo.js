14990a0e0cc2b9b8e2d455a0f3621de21dab048c
李宗校 <158638447@qq.com>
2022-08-27 14:43:27 +0800

    注释

--- demo.js
+++ demo.js
@@ -8,23 +8,23 @@
 // @match        *://*.zte.com.cn/*
 // ==/UserScript==
 
-(function () {
-  "use strict";
-  var button = document.createElement("button"); //创建一个按钮
-  button.textContent = "一键开启躺平学习模式"; //按钮内容
-  button.style.width = "200px"; //按钮宽度
-  button.style.height = "28px"; //按钮高度
-  button.style.textAlign = "center !important"; //文本居中
-  button.style.lineHeight = "25px"; //文本行高
-  button.style.color = "white"; //按钮文字颜色
-  button.style.background = "#e33e33"; //按钮底色
-  button.style.border = "1px solid #e33e33"; //边框属性
-  button.style.borderRadius = "4px"; //按钮四个角弧度
-  button.addEventListener("click", clickBotton); //监听按钮点击事件
-  function clickBotton() {
-    alert('开始准备开始躺平学习模式 ')
-    preventDefult();
-  }
-  var like_comment = document.getElementById("divHead2"); //getElementsByClassName 返回的是数组，所以要用[] 下标
-    like_comment.appendChild(button); //把按钮加入到 x 的子节点中
-})();
+// (function () {
+//   "use strict";
+//   var button = document.createElement("button"); //创建一个按钮
+//   button.textContent = "一键开启躺平学习模式"; //按钮内容
+//   button.style.width = "200px"; //按钮宽度
+//   button.style.height = "28px"; //按钮高度
+//   button.style.textAlign = "center !important"; //文本居中
+//   button.style.lineHeight = "25px"; //文本行高
+//   button.style.color = "white"; //按钮文字颜色
+//   button.style.background = "#e33e33"; //按钮底色
+//   button.style.border = "1px solid #e33e33"; //边框属性
+//   button.style.borderRadius = "4px"; //按钮四个角弧度
+//   button.addEventListener("click", clickBotton); //监听按钮点击事件
+//   function clickBotton() {
+//     alert('开始准备开始躺平学习模式 ')
+//     preventDefult();
+//   }
+//   var like_comment = document.getElementById("divHead2"); //getElementsByClassName 返回的是数组，所以要用[] 下标
+//     like_comment.appendChild(button); //把按钮加入到 x 的子节点中
+// })();
